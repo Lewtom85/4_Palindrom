b1b2cc977c5152051820a43d2d9b77f2b0a38b05
Tomasz Lewandowski <lewtom85@gmail.com>
2023-07-16 12:12:35 +0200

    Three versions

--- Palindrom.py
+++ Palindrom.py
@@ -1,4 +1,15 @@
 #Palindrom
+# 1. Wersja z Rafalem. Dziala poprawnie
+text = ("kajak")
+
+def palindrom(text):
+    text = text.lower()
+    return text == text[::-1]
+
+print(palindrom(text))
+print(text)
+
+#2. Wersja z internetu z bledem.
 def isPalindrome(s):
     if len(s) <= 1 :
         return True
@@ -15,3 +26,14 @@ for i in Palindrome_input:
         print("The given string ", "'", i, "'", "is a palindrome")
     else:
         print("The given string ", "'", i, "'", "is not a palindrome")
+
+# 3. Wersja internet.
+a_string = ("potop")
+
+def palindrome(a_string):
+    a_string = a_string.lower().replace(' ', '')
+    reversed_string = ''
+    for i in range(len(a_string), 0, -1):
+        reversed_string += a_string[i-1]
+    return a_string == reversed_string
+print(palindrome(a_string))
